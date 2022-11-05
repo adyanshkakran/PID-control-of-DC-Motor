6c5bc7546cd164badf14fb1a0df20203ba5efb29
adyanshkakran <adyanshkakran@gmail.com>
2022-11-05 15:26:09 +0530

    Made timer perfect

--- backend/static/experiment/scripts.js
+++ backend/static/experiment/scripts.js
@@ -5,15 +5,24 @@ function changeSlider(){
 
 setTimeout(() => {
     location.href = '../../logout'
-}, 15*60*1000);
+}, parseInt(localStorage.getItem("timeLeft"))*1000);
 
-let timeLeft = 15*60;
-setInterval(() => {
-    timeLeft--
-    let min = Math.floor(timeLeft/60),sec = timeLeft%60;
-    if(sec < 10)
-        sec = "0"+sec
-    if(min < 10)
-        min = "0"+min
-    document.getElementById("time-left").innerHTML = `${min}:${sec}`
-}, 1000);
+function updateTime() {
+    timeLeft = parseInt(localStorage.getItem("timeLeft"))
+    if(!isNaN(timeLeft)){
+        timeLeft--
+        let min = Math.floor(timeLeft/60),sec = timeLeft%60;
+        if(sec < 10)
+            sec = "0"+sec
+        if(min < 10)
+            min = "0"+min
+        document.getElementById("time-left").innerHTML = `${min}:${sec}`
+        localStorage.setItem("timeLeft",timeLeft)
+    }else{
+        localStorage.setItem("timeLeft","900")
+        document.getElementById("time-left").innerHTML = `15:00`
+    }
+}
+
+updateTime()
+setInterval(updateTime, 1000);
