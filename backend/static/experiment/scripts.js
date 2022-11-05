c73e1b422a10c0121ed43958bd575b6500fde501
adyanshkakran <adyanshkakran@gmail.com>
2022-11-05 14:54:12 +0530

    Added Timer

--- backend/static/experiment/scripts.js
+++ backend/static/experiment/scripts.js
@@ -2,3 +2,14 @@ function changeSlider(){
     let kp = document.getElementById("kp")
     
 }
+
+setTimeout(() => {
+    location.href = '../../logout'
+}, 15*60*1000);
+
+let timeLeft = 15*60;
+setInterval(() => {
+    timeLeft--
+    let min = Math.floor(timeLeft/60),sec = timeLeft%60;
+    document.getElementById("time-left").innerHTML = `${min}:${sec}`
+}, 1000);
