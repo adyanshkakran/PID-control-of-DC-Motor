67e0fda64a83ed93a215de679b21f059cc078cd4
adyanshkakran <adyanshkakran@gmail.com>
2022-11-05 15:03:09 +0530

    Added 0 to timer

--- backend/static/experiment/scripts.js
+++ backend/static/experiment/scripts.js
@@ -11,5 +11,9 @@ let timeLeft = 15*60;
 setInterval(() => {
     timeLeft--
     let min = Math.floor(timeLeft/60),sec = timeLeft%60;
+    if(sec < 10)
+        sec = "0"+sec
+    if(min < 10)
+        min = "0"+min
     document.getElementById("time-left").innerHTML = `${min}:${sec}`
 }, 1000);
