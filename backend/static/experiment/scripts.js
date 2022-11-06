b793fae3e1c3be233868991e54944d7c22d5f575
adyanshkakran <adyanshkakran@gmail.com>
2022-11-07 00:01:02 +0530

    Added slider functionality

--- backend/static/experiment/scripts.js
+++ backend/static/experiment/scripts.js
@@ -10,6 +10,8 @@ setTimeout(() => {
 function updateTime() {
     timeLeft = parseInt(localStorage.getItem("timeLeft"))
     if(!isNaN(timeLeft)){
+        if(timeLeft < 0)
+            window.location.href = "../../logout"
         timeLeft--
         let min = Math.floor(timeLeft/60),sec = timeLeft%60;
         if(sec < 10)
@@ -26,3 +28,57 @@ function updateTime() {
 
 updateTime()
 setInterval(updateTime, 1000);
+
+$('.input').keypress(function (e) {
+    if (e.which == 13) {
+         e.preventDefault();
+    }
+});
+
+function bind_to_slider(slider,input,min,max,from,step){
+    let $range = $(slider);
+    let $input = $(input);
+    
+    $range.ionRangeSlider({
+        skin: "round",
+        // type: "single",
+        min: min,
+        max: max,
+        from: from,
+        step: step,
+        onStart: (data) => {
+            $input.prop("value", data.from);
+        },
+        onChange: (data) => {
+            $input.prop("value", data.from);
+        }
+    });
+    
+    let instance = $range.data("ionRangeSlider");
+    
+    $input.on("input", function() {
+        var val = $(this).prop("value");
+        if (val < min) {
+            val = min;
+        } else if (val > max) {
+            val = max;
+        }
+    
+        instance.update({
+            from: val
+        });
+    });
+}
+
+bind_to_slider(".kp-slider",".kp-input",-100,100,0,0,0.1);
+bind_to_slider(".ki-slider",".ki-input",-100,100,0,0,0.1);
+bind_to_slider(".kd-slider",".kd-input",-100,100,0,0,0.1);
+bind_to_slider(".angle-slider",".angle-input",0,360,180,1);
+
+function startSim() {
+    let kp = $(".kp-input").val();
+    let ki = $(".ki-input").val();
+    let kd = $(".kd-input").val();
+    let angle = $(".angle-input").val();
+    console.log(kp,ki,kd,angle)
+}
