501f9c5957f1acb12dbec1761b0209e665807de4
adyanshkakran <adyanshkakran@gmail.com>
2022-11-19 17:25:59 +0530

    Added graph functionality

--- backend/sample.js
+++ backend/sample.js
@@ -3,9 +3,7 @@ import nodemailer from 'nodemailer';
 import generator from 'generate-password';
 import { createClient } from 'redis';
 import sessions from 'express-session';
-import ensureAuthenticated  from 'connect-ensure-authenticated';
 import cookieParser from 'cookie-parser';
-import  Express  from 'express';
 import http from 'https';
 import  express  from 'express';
 
